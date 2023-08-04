ccaecf1aa25d8aada6a1c5ef71d4b8b240108600
Ralph0210 <ralphchang@utexas.edu>
2023-08-04 15:56:53 -0500

    navbar setup

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,29 @@
 import React from 'react'
-import NavbarButton from './NavbarButton'
+import './Navbar.css'
+import { useState } from 'react'
+import {Routes, Route, Link} from 'react-router-dom';
 
 const Navbar = () => {
-  return (
-    <div>NavbarButton</div>
-  )
+    const Nav = () => (
+        <ul className='nav_list'>
+                <li>Home</li>
+                <li>Analysis</li>
+                <li>Explore Data</li>
+                <li>About</li>
+                <li>Search</li>
+                <li>EN</li>
+        </ul>
+    )
+      return (
+        <nav>
+            <div className='logo_container'>
+            <li>SCF</li>
+            </div>
+            <div className='nav_list_container'>
+                <Nav />
+            </div>
+        </nav>
+      )
 }
 
 export default Navbar
