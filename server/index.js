b8c38026dd521b598b73ccb160830d89284049c3
Ralph0210 <ralphchang@utexas.edu>
2023-09-09 17:16:19 -0500

    sequelize setup

--- server/index.js
+++ server/index.js
@@ -1,58 +1,40 @@
-//setup
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const mysql = require('mysql2');
-
-app.use(express.json())
-app.use(cors())
-
+const express = require('express');
+const cors = require('cors');
 const db = require('./models')
+const { s2019, s2016, s2013, s2010 } = require('./models'); // Import your Sequelize models
 
+const app = express();
+const port = 3001;
 
-const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root', // Replace with your MySQL username
-    password: 'Rain0210', // Replace with your MySQL password
-    database: 'scf', // Replace with your MySQL database name
+app.use(express.json());
+app.use(cors());
+
+// Define a route to retrieve the first five HHSEX values from the s2019 model
+app.get('/getFirstFiveHHSEX', async (req, res) => {
+    try {
+      // Query s2019 to get the first 5 HHSEX values
+      const s2019Data = await s2013.findAll({
+        attributes: ['HHSEX'],
+        limit: 5,
+      });
+  
+      // Format the data as an object with a 'data' property
+      const responseData = {
+        year: 2019,
+        data: s2019Data,
+      };
+  
+      // Respond with the formatted data as JSON
+      res.json(responseData);
+    } catch (error) {
+      // Handle errors
+      console.error('Error:', error);
+      res.status(500).json({ error: 'An error occurred' });
+    }
   });
-
-// const Model2019s = require('./models/2019s')
-
-// app.get('/query2019s', async (req, res) => {
-//     try {
-//       // Query the first 5 rows of the 2019s table and select the HHSEX variable
-//       const result = await Model2019s.findAll({
-//         attributes: ['HHSEX'],
-//         limit: 5,
-//       });
-  
-//       res.json(result);
-//     } catch (error) {
-//       console.error(error);
-//       res.status(500).json({ error: 'An error occurred' });
-//     }
-//   });
-  
-  app.get('/fetchData', (req, res) => {
-    const query = 'SELECT HHSEX FROM s2019 LIMIT 5'; // Replace 'your_table' with your actual table name
-  
-    connection.query(query, (err, results) => {
-      if (err) {
-        console.error('Error executing query:', err);
-        res.status(500).json({ error: 'An error occurred' });
-      } else {
-        res.json(results);
-      }
-    });
-  });
-  
-
-
 
 db.sequelize.sync().then(() => {
     app.listen(3001, () => {
-        console.log("Server running on port 3001")
+        console.log(`Server is running on http://localhost:${port}`)
     })
 })
-
