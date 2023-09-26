d53c8123663285763b9c56e520b44ec949485fe7
Ralph0210 <ralphchang@utexas.edu>
2023-09-26 14:45:39 -0500

    nodes animation done

--- client/src/components/ExploreData/ExploreData.js
+++ client/src/components/ExploreData/ExploreData.js
@@ -1,41 +1,42 @@
 import React, { useState, useEffect, useRef } from "react";
 import * as d3 from "d3";
 import "./ExploreData.css";
-import {INITIAL_VALUE, ReactSVGPanZoom, TOOL_AUTO} from 'react-svg-pan-zoom';
+import { INITIAL_VALUE, ReactSVGPanZoom, TOOL_AUTO } from "react-svg-pan-zoom";
 // import data from './flare-2.json'
+import data from "./var.json";
 
 const ExploreData = () => {
   // const [data] = useState([25, 50, 35, 15, 94, 50]);
   const Viewer = useRef(null);
-  const [tool, setTool] = useState(TOOL_AUTO)
-  const [value, setValue] = useState(INITIAL_VALUE)
+  const [tool, setTool] = useState(TOOL_AUTO);
+  const [value, setValue] = useState(INITIAL_VALUE);
   const svgRef = useRef();
   const svgRef2 = useRef();
-  const data = {
-    children: [
-      {
-        name: "boss1",
-        children: [
-          { name: "mister_a", colname: "level3" },
-          { name: "mister_b", colname: "level3" },
-          { name: "mister_c", colname: "level3" },
-          { name: "mister_d", colname: "level3" },
-        ],
-        colname: "level2",
-      },
-      {
-        name: "boss2",
-        children: [
-          { name: "mister_e", colname: "level3" },
-          { name: "mister_f", colname: "level3" },
-          { name: "mister_g", colname: "level3" },
-          { name: "mister_h", colname: "level3" },
-        ],
-        colname: "level2",
-      },
-    ],
-    name: "CEO",
-  };
+  // const data = {
+  //   children: [
+  //     {
+  //       name: "boss1",
+  //       children: [
+  //         { name: "mister_a", colname: "level3" },
+  //         { name: "mister_b", colname: "level3" },
+  //         { name: "mister_c", colname: "level3" },
+  //         { name: "mister_d", colname: "level3" },
+  //       ],
+  //       colname: "level2",
+  //     },
+  //     {
+  //       name: "boss2",
+  //       children: [
+  //         { name: "mister_e", colname: "level3" },
+  //         { name: "mister_f", colname: "level3" },
+  //         { name: "mister_g", colname: "level3" },
+  //         { name: "mister_h", colname: "level3" },
+  //       ],
+  //       colname: "level2",
+  //     },
+  //   ],
+  //   name: "CEO",
+  // };
   // useEffect(() => {
   //   Viewer.current.fitToViewer();
   // }, []);
@@ -95,128 +96,196 @@ const ExploreData = () => {
   // }, [data]);
 
   useEffect(() => {
-  // Specify the chart’s dimensions.
-  const width = 800;
-  const height = width;
-  const cx = width * 0.5; // adjust as needed to fit
-  const cy = height * 0.59; // adjust as needed to fit
-  const radius = Math.min(width, height) / 2 - 30;
+    // Specify the chart’s dimensions.
+    const width = 800;
+    const height = width;
+    const cx = width * 0.5; // adjust as needed to fit
+    const cy = height * 0.59; // adjust as needed to fit
+    const radius = Math.min(width, height) / 2 - 30;
 
-  // Create a radial tree layout. The layout’s first dimension (x)
-  // is the angle, while the second (y) is the radius.
-  const tree = d3.tree()
+    // Create a radial tree layout. The layout’s first dimension (x)
+    // is the angle, while the second (y) is the radius.
+    const tree = d3
+      .tree()
       .size([2 * Math.PI, radius])
       .separation((a, b) => (a.parent == b.parent ? 1 : 2) / a.depth);
 
-  // Sort the tree and apply the layout.
-  const root = tree(d3.hierarchy(data)
-      .sort((a, b) => d3.ascending(a.data.name, b.data.name)));
+    // Sort the tree and apply the layout.
+    const root = tree(
+      d3.hierarchy(data).sort((a, b) => d3.ascending(a.data.name, b.data.name))
+    );
 
-  // Creates the SVG container.
-  const svg = d3.select(svgRef2.current)
+    // Creates the SVG container.
+    const svg = d3
+      .select(svgRef2.current)
       .attr("width", width)
       .attr("height", height)
       .attr("viewBox", [-cx, -cy, width, height])
-      .attr("style", `width: ${width}px; height: ${height}px; font: 10px sans-serif;`)
+      .attr(
+        "style",
+        `width: ${width}px; height: ${height}px; font: 10px sans-serif;`
+      );
 
-
-  // Append links.
-  let links = svg.append("g")
+    // Append links.
+    let links = svg
+      .append("g")
       .attr("fill", "none")
       .attr("stroke", "#555")
       .attr("stroke-opacity", 0.4)
       .attr("stroke-width", 1.5)
-    .selectAll()
-    .data(root.links())
-    .join("path")
-      .attr("d", d3.linkRadial()
-          .angle(d => d.x)
-          .radius(d => d.y));
+      .selectAll()
+      .data(root.links())
+      .join("path")
+      .attr(
+        "d",
+        d3
+          .linkRadial()
+          .angle((d) => d.x)
+          .radius((d) => d.y)
+      );
 
-// Append nodes.
-let nodes = svg
-  .append("g")
-  .selectAll()
-  .data(root.descendants())
-  .join("circle")
-  .attr("transform", (d) => `rotate(${(d.x * 180) / Math.PI - 90}) translate(${d.y},0)`)
-  .attr("fill", (d) => (d.children ? "#555" : "#999"))
-  .attr("r", 10)
-  .attr("class", "circle");
+    let nodeAnimation2 = svg
+      .append("g")
+      .selectAll()
+      .data(root.descendants())
+      .join("circle")
+      .attr(
+        "transform",
+        (d) => `rotate(${(d.x * 180) / Math.PI - 90}) translate(${d.y},0)`
+      )
+      .attr("fill", (d) => (d.children ? "#555" : "#999"))
+      .attr("r", 0)
+      .attr("class", "enlarge-circle");
 
-// Append labels.
-let labels = svg
-  .append("g")
-  .attr("stroke-linejoin", "round")
-  .attr("stroke-width", 3)
-  .selectAll()
-  .data(root.descendants())
-  .join("text")
-  .attr("transform", (d) => `rotate(${(d.x * 180) / Math.PI - 90}) translate(${d.y},0) rotate(${d.x >= Math.PI ? 180 : 0})`)
-  .attr("dy", "0.31em")
-  .attr("x", (d) => (d.x < Math.PI === !d.children ? 6 : -6))
-  .attr("text-anchor", (d) => (d.x < Math.PI === !d.children ? "start" : "end"))
-  .attr("paint-order", "stroke")
-  .attr("stroke", "white")
-  .attr("fill", "currentColor")
-  .text((d) => d.data.name)
-  .style("opacity", 0); // Initially set text opacity to 0
+    let nodeAnimation = svg
+      .append("g")
+      .selectAll()
+      .data(root.descendants())
+      .join("circle")
+      .attr(
+        "transform",
+        (d) => `rotate(${(d.x * 180) / Math.PI - 90}) translate(${d.y},0)`
+      )
+      .attr("fill", (d) => (d.children ? "#555" : "#999"))
+      .attr("r", 0)
+      .attr("class", "enlarge-circle");
 
+    // Append nodes.
+    let nodes = svg
+      .append("g")
+      .selectAll()
+      .data(root.descendants())
+      .join("circle")
+      .attr(
+        "transform",
+        (d) => `rotate(${(d.x * 180) / Math.PI - 90}) translate(${d.y},0)`
+      )
+      .attr("fill", (d) => (d.children ? "#555" : "#999"))
+      .attr("r", 10)
+      .attr("class", "circle");
 
-  // Define mouseover and mouseout event handlers for nodes
-nodes
-.on("mouseover", function (d, i) {
-  console.log("Mouseover Event - Data:", d, "Index:", i);
-  // Change the circle size
-  d3.select(this)
-    .transition()
-    .duration(400)
-    .attr("r", 20);
+    // Append labels.
+    let labels = svg
+      .append("g")
+      .attr("stroke-linejoin", "round")
+      .attr("stroke-width", 3)
+      .selectAll()
+      .data(root.descendants())
+      .join("text")
+      .attr(
+        "transform",
+        (d) =>
+          `rotate(${(d.x * 180) / Math.PI - 90}) translate(${d.y},0) rotate(${
+            d.x >= Math.PI ? 180 : 0
+          })`
+      )
+      .attr("dy", "0.31em")
+      .attr("x", (d) => (d.x < Math.PI === !d.children ? 6 : -6))
+      .attr("text-anchor", (d) =>
+        d.x < Math.PI === !d.children ? "start" : "end"
+      )
+      .attr("paint-order", "stroke")
+      .attr("stroke", "white")
+      .attr("fill", "currentColor")
+      .text((d) => d.data.name)
+      .style("opacity", 0); // Initially set text opacity to 0
 
-  // Change the opacity of the associated text to 1
-  labels
-    .filter((textData) => textData.data.name === i.data.name) // Filter for the matching text element
-    .transition()
-    .duration(800)
-    .style("opacity", 1);
-})
-.on("mouseout", function (d, i) {
-  // Change the circle size back to its original size
-  d3.select(this)
-    .transition()
-    .duration(800)
-    .attr("r", 10);
+    // Define mouseover and mouseout event handlers for nodes
+    nodes
+      .on("mouseover", function (d, i) {
+        console.log("Mouseover Event - Data:", d, "Index:", i);
+        // Change the circle size
 
-  // Change the opacity of the associated text back to 0
-  labels
-    .filter((textData) => textData.data.name === i.data.name) // Filter for the matching text element
-    .transition()
-    .duration(800)
-    .style("opacity", 0);
-});
+        nodeAnimation
+          .filter((data) => data.data === i.data) // Filter for the matching data point
+          .transition()
+          .duration(500)
+          .attr("r", 15)
+          .style("opacity", 0.5);
+
+        nodeAnimation2
+          .filter((data) => data.data === i.data) // Filter for the matching data point
+          .transition()
+          .duration(600)
+          .attr("r", 20)
+          .style("opacity", 0.3);
+
+        // Change the opacity of the associated text to 1
+        labels
+          .filter((textData) => textData.data.name === i.data.name) // Filter for the matching text element
+          .transition()
+          .duration(800)
+          .style("opacity", 1);
+      })
+      .on("mouseout", function (d, i) {
+        // Change the circle size back to its original size
+        nodeAnimation
+          .filter((data) => data.data === i.data) // Filter for the matching data point
+          .transition()
+          .duration(600)
+          .attr("r", 0)
+          .style("opacity", 0.5);
+
+        nodeAnimation2
+          .filter((data) => data.data === i.data) // Filter for the matching data point
+          .transition()
+          .duration(600)
+          .attr("r", 0)
+          .style("opacity", 0.5);
+
+        // Change the opacity of the associated text back to 0
+        labels
+          .filter((textData) => textData.data.name === i.data.name) // Filter for the matching text element
+          .transition()
+          .duration(800)
+          .style("opacity", 0);
+      });
   }, []);
 
   return (
     <div>
       <ReactSVGPanZoom
         ref={Viewer}
-        background='rgba(217, 217, 217, 0.20)'
-        defaultTool='pan'
-        width={1440} height={1024}
-        tool={tool} onChangeTool={setTool}
-        value={value} onChangeValue={setValue}
+        background="rgba(217, 217, 217, 0.20)"
+        defaultTool="pan"
+        width={1440}
+        height={1024}
+        tool={tool}
+        onChangeTool={setTool}
+        value={value}
+        onChangeValue={setValue}
         detectAutoPan={false}
         toolbarProps={{
-            position: "none", // Set position to "none" to hide the toolbar
-          }}
-          miniatureProps={{
-            position: "none", // Set position to "none" to hide the miniature
-          }}
-          className='map_parent_container'
+          position: "none", // Set position to "none" to hide the toolbar
+        }}
+        miniatureProps={{
+          position: "none", // Set position to "none" to hide the miniature
+        }}
+        className="map_parent_container"
       >
-      <svg >
-        <g ref={svgRef2}></g>
-      </svg>
+        <svg>
+          <g ref={svgRef2}></g>
+        </svg>
       </ReactSVGPanZoom>
     </div>
   );
