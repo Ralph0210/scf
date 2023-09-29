364e05c0a376c78d6e7539e6c67e51d3955c9a97
Ralph0210 <ralphchang@utexas.edu>
2023-09-29 18:42:46 -0500

    using framer motion for animation

--- client/src/components/Instruction1/Instruction1.js
+++ client/src/components/Instruction1/Instruction1.js
@@ -1,22 +1,16 @@
 import React from 'react'
 import './Instruction1.css'
+import { motion, useScroll } from "framer-motion"
+import InstructionMap from '../InstructionMap/InstructionMap'
 
 const Instruction1 = () => {
+
   return (
     <div className='container'>
         <div className='title'>
             <h1>What is the SCF</h1>
         </div>
-
-        <div className='content'>
-            <div className='instruction_map'></div>
-            <div className='description'>
-                <h2>Comprehensive <span className='new-line'>Financial Survey.</span></h2>
-                <div className='details'>
-                    <p>The SCF, conducted by the Federal Reserve System, is a nationwide survey that provides a comprehensive view of income, wealth, debt, and various financial factors.</p>
-                </div>
-            </div>
-        </div>
+        <InstructionMap />
     </div>
   )
 }
---
+++ client/src/components/InstructionMap/InstructionMap.js
@@ -0,0 +1,31 @@
+import React, {useRef} from 'react';
+import './InstructionMap.css';
+import { motion} from 'framer-motion';
+import ExploreData2 from '../ExploreData/ExploreData2'
+
+const InstructionMap = () => {
+
+  // Animation variants
+  const fadeIn = {
+    hidden: { opacity: 0, y: 20 }, // Start with opacity 0 and slight Y translation
+    visible: { opacity: 1, y: 0, transition: { duration: 1 } }, // Fade in and move up smoothly
+  };
+
+  return (
+    <div className="container">
+      <div className='content'>
+            <motion.div className='instruction_map' variants={fadeIn} initial='hidden' whileInView='visible'><ExploreData2/></motion.div>
+            <motion.div className='description' variants={fadeIn} initial='hidden' whileInView='visible'>
+                <h2>Comprehensive <span className='new-line'>Financial Survey.</span></h2>
+                <motion.div className='details' variants={fadeIn} initial='hidden' whileInView='visible'>
+                    <p>The SCF, conducted by the Federal Reserve System, is a nationwide survey that provides a comprehensive view of income, wealth, debt, and various financial factors.</p>
+                </motion.div>
+            </motion.div>
+        </div>
+
+      {/* Add more text elements and animations as needed */}
+    </div>
+  );
+};
+
+export default InstructionMap;
