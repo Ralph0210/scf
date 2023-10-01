4dc613f6ae64f4721551a920c15cf1c5fc90dd1b
Ralph0210 <ralphchang@utexas.edu>
2023-09-30 19:02:25 -0500

    stagger animation setup

--- client/src/components/Hero/Hero.js
+++ client/src/components/Hero/Hero.js
@@ -1,30 +1,42 @@
-import React from 'react'
-import './Hero.css'
+import React from "react";
+import "./Hero.css";
+import { motion } from "framer-motion";
 
-const Hero = ({InstructionRef}) => {
-    const handleClick = () => {
-        InstructionRef.current.scrollIntoView({behavior: 'smooth'})
-    }
+const Hero = ({ InstructionRef }) => {
+  const handleClick = () => {
+    InstructionRef.current.scrollIntoView({ behavior: "smooth" });
+  };
   return (
-    <div className='cta_container'>
-        <div className='cta_left_container'>
-        <div className='header'>
-            <h1>Unlock Financial Insights</h1>
+    <div className="cta_container">
+      <div className="cta_left_container">
+        <div className="header">
+          <h1>Unlock Financial Insights</h1>
         </div>
-        <div className='intro'>
-            <p>Discover the Power of the Survey of Consumer Finance (SCF) and Make Informed Financial Decisions. Access a Wealth of Data on Income, Wealth, Debt, and more. Gain Deep Understanding, Identify Trends, and Plan for a Strong Financial Future.</p>
+        <div className="intro">
+          <p>
+            Discover the Power of the Survey of Consumer Finance (SCF) and Make
+            Informed Financial Decisions. Access a Wealth of Data on Income,
+            Wealth, Debt, and more. Gain Deep Understanding, Identify Trends,
+            and Plan for a Strong Financial Future.
+          </p>
         </div>
-        <div className='cta_button'>
-            <button onClick={handleClick}>Learn More</button>
+        <div className="cta_button">
+          <motion.button
+            whileHover={{scale:1.2}}
+            whileTap={{ scale: 0.6 }}
+            onClick={handleClick}
+          >
+            Learn More
+          </motion.button>
         </div>
-        </div>
+      </div>
 
-        <div className='right_container'>
-            <img src='/cta.png' className='hero_img'/>
-            <div className='background'></div>
-        </div>
+      <div className="right_container">
+        <img src="/cta.png" className="hero_img" />
+        <div className="background"></div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
--- client/src/components/Interest/Interest.js
+++ client/src/components/Interest/Interest.js
@@ -1,12 +1,35 @@
-import React from 'react'
+import React, {useEffect, useRef} from 'react'
 import './Interest.css'
+import {motion, stagger, useInView, animate} from 'framer-motion'
 
 const Interest = () => {
+  const assetRef = useRef(null)
+  const assetIsInView = useInView(assetRef)
+
+  // const staggerItems = stagger(0.1, { from: "first" });
+
+  // useEffect(() => {
+  //   animate(
+  //     ".interest_left_container li",
+  //     assetIsInView
+  //       ? { opacity: 1, scale: 1}
+  //       : { opacity: 0, scale: 0},
+  //     {
+  //       duration: 0.5,
+  //       delay: assetIsInView ? staggerItems : 0
+  //     }
+  //   );
+  // },[assetIsInView])
+
   return (
     <div className='interest_container'>
         <div className='interest_left_container'>
             <h2>Assets</h2>
-            <ul>
+            <motion.ul
+            ref={assetRef}
+            >
+                <motion.li whileHover={{scale:1.2}}
+             whileTap={{ scale: 0.8 }} transition={{ ease: "easeOut", duration: 1}}>Lorem</motion.li>
                 <li>Lorem</li>
                 <li>Lorem</li>
                 <li>Lorem</li>
@@ -15,8 +38,7 @@ const Interest = () => {
                 <li>Lorem</li>
                 <li>Lorem</li>
                 <li>Lorem</li>
-                <li>Lorem</li>
-            </ul>
+            </motion.ul>
         </div>
         <div className='interest_right_container'>
             <img src='/asset.png' />
